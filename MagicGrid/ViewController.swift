48d725951a981ccbec053864c899a095f834ded1
OscarF95 <o.denaf@gmail.com>
2017-05-21 23:17:45 -0700

    Initial Commit

---
+++ MagicGrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicGrid
+//
+//  Created by Oscar Dena on 21/05/17.
+//  Copyright © 2017 ___OscarD___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
